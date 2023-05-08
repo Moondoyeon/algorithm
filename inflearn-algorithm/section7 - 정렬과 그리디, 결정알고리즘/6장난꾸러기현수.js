3a222260b8402864c968fa5fb824137314ce6fea
Moondoyeon <mmmdo21@gmail.com>
2023-05-08 13:50:36 +0900

    mischief

---
+++ inflearn-algorithm/section7 - 정렬과 그리디, 결정알고리즘/6장난꾸러기현수.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  let answer = [];
+  let sortArr = arr.slice(); // 깊은복사 (단 중첩 객체인 데이터가 있다면 얕은복사)
+  sortArr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== sortArr[i]) answer.push(i + 1);
+  }
+  return answer;
+}
+
+let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+console.log(solution(arr));
