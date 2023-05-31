ff847dc8156d4dab38740afa9391a58ac87e3230
Moondoyeon <mmmdo21@gmail.com>
2023-05-31 12:13:17 +0900

    add annotation

--- inflearn-algorithm/section8 - 재귀함수와 완전탐색(깊이우선탐색,DFS)/13수열 추측하기.js
+++ inflearn-algorithm/section8 - 재귀함수와 완전탐색(깊이우선탐색,DFS)/13수열 추측하기.js
@@ -2,8 +2,8 @@ function solution(n, f) {
   let answer,
     flag = 0;
   let dy = Array.from(Array(11), () => Array(11).fill(0));
-  let ch = Array.from({ length: n + 1 }, () => 0);
   let b = Array.from({ length: n }, () => 0); // 조합
+  let ch = Array.from({ length: n + 1 }, () => 0);
   let p = Array.from({ length: n }, () => 0); // 순열
   function combi(n, r) {
     if (dy[n][r] > 0) return dy[n][r];
@@ -12,20 +12,22 @@ function solution(n, f) {
   }
   function DFS(L, sum) {
     if (flag) return;
+    if (L === n && sum !== f) return;
     if (L === n && sum === f) {
       answer = p.slice();
       flag = 1;
     } else {
       for (let i = 1; i <= n; i++) {
         if (ch[i] === 0) {
-          ch[i] = 1;
-          p[L] = i;
-          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 1; // 0 1 이 원소
+          p[L] = i; // 1 2 3 4 이 원소
+          DFS(L + 1, sum + b[L] * p[L]); // 뎁스(L)마다 곱하는 순열 원소는 달라지지.
           ch[i] = 0;
         }
       }
     }
   }
+  // 조합 만들기 (한 번 만들고 불변)
   for (let i = 0; i < n; i++) {
     b[i] = combi(n - 1, i);
   }
